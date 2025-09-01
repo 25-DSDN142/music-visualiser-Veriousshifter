e3c74b36b9999101b8842bf743d8568f8fef9056
Veriousshifter <Veriousshifter@gmail.com>
2025-09-02 06:22:09 +0800

    test

--- music_starter.js
+++ music_starter.js
@@ -1,43 +1,67 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
-  textFont('Verdana'); // please use CSS safe fonts
-  rectMode(CENTER)
-  textSize(24);
+  colorMode(RGB,255);
+  background(255);
+  strokeWeight(5);
+  stroke(drum,80,80);
+
+  var drumMap =parseInt(map(drum,0,100,30,90));
+  var lengthOfLine =300;
+  var lineStart = 200;
+  var lineEnd = lineStart + lengthOfLine;
+
+  fill (102,102,102)
+  line(150,300,lineStart,400);
+  line(550,300,lineEnd,400);
+  beginShape();
+  vertex(150,200)
+  vertex(150,150)
+  quadraticVertex(350, 50, 550, 150);
+  vertex(550,200);
+  endShape();
+
+  beginShape();
+  vertex(200,500);
+  vertex(200,550);
+  quadraticVertex(350, 650, 500, 550);
+  vertex(500,500);
+  endShape();
+
   
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
- 
-// changes 
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
+  beginShape();
+  vertex(150,300);
+  quadraticVertex(350, 350, 550, 300);
+  vertex(570,250);
+  vertex(550,200);
+  quadraticVertex(350, 250, 150, 200);
+  vertex(130,250);
+  vertex(150,300);
+  endShape();
+
+  fill(drum,80,80);
+  for (let i = 0; i < 6; i++) {
+    line(200+(i*60),410,200+(i*60),500);
+  }
+  line(lineStart,500,lineEnd,500);
+  line(lineStart,400,lineEnd,400);
+  line(lineStart,410,lineEnd,410);
+  line(lineStart,390,lineEnd,390);
+  ellipse(lineStart,400,30,30);
+  ellipse(lineEnd,400,30,30);
+
+
+let eyesize = map(bass,0,100,40,80);
+  stroke(bass,80,80);
+  strokeWeight(3);
+  fill(5,217,232)
+  for (let i = 0; i < 2; i++) {
+      ellipse(170+(i*88),255+(i*15),eyesize,eyesize);
+   }
+  ellipse(350,275,eyesize,eyesize);
+  ellipse(442,270,eyesize,eyesize);
+  ellipse(530,255,eyesize,eyesize);
+  
+
+  
 }
